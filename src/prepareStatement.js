16bd115ac262303a9c8d595fae748d6ecd601c3d
Ron Kok <ronkok@comcast.net>
2021-04-22 10:08:14 -0700

    Revert previous commit.

--- src/prepareStatement.js
+++ src/prepareStatement.js
@@ -249,10 +249,7 @@ export const prepareStatement = (inputStr, decimalFormat = "1,000,000.") => {
     eqn = parse(mainStr, decimalFormat)
     if (mustAlign) {
       eqn = "\\begin{aligned}" + eqn
-      const pos = eqn.indexOf("=")
-      if (eqn.charAt(pos - 1) !== "&") {
-        eqn = eqn.slice(0, pos) + "&" + eqn.slice(pos)
-      }
+      eqn = eqn.slice(0, pos) + "&" + eqn.slice(pos)
     }
     const alignChar = mustAlign ? "\\\\ &" : ""
     altEqn = mainStr
