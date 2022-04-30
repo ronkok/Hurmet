a3ab8357ffd8f4b609fa8417032e16b72d773509
Ron Kok <ronkok@comcast.net>
2022-04-30 14:50:51 -0700

    Revert to using Temml in Firefox

--- src/schema.js
+++ src/schema.js
@@ -304,18 +304,22 @@ export const nodes = {
         dom.firstChild.textContent = node.attrs.alt ? node.attrs.alt : node.attrs.entry
       } else {
         const tex = node.attrs.tex
-        /*temml.render(tex, dom, {
-          displayMode: node.attrs.displayMode,
-          trust: (context) => context.command === '\\class' && context.class === "special-fraction"
-        })*/
-        katex.render(tex, dom, {
-          displayMode: node.attrs.displayMode,
-          strict: false,
-          macros: {"\\class": "\\htmlClass"},
-          throwOnError: false,
-          minRuleThickness: 0.06,
-          trust: (context) => context.command === '\\htmlClass' && context.class === "special-fraction"
-        })
+        const isFF = 'MozAppearance' in document.documentElement.style
+        if (isFF) {
+          temml.render(tex, dom, {
+            displayMode: node.attrs.displayMode,
+            trust: (context) => context.command === '\\class' && context.class === "special-fraction"
+          })
+        } else {
+          katex.render(tex, dom, {
+            displayMode: node.attrs.displayMode,
+            strict: false,
+            macros: {"\\class": "\\htmlClass"},
+            throwOnError: false,
+            minRuleThickness: 0.06,
+            trust: (context) => context.command === '\\htmlClass' && context.class === "special-fraction"
+          })
+        }
       }
       // Before writing to DOM, I filter out most of the run-time info in node.attrs.
       dom.dataset.entry = node.attrs.entry
