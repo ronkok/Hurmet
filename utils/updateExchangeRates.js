0b8a94a1339ed658dfd5d55164d4ed1b92792913
Ron Kok <ronkok@comcast.net>
2022-05-13 10:43:40 -0700

    Move exchange rates to html file

--- utils/updateExchangeRates.js
+++ utils/updateExchangeRates.js
@@ -1,6 +1,6 @@
 const fs = require("fs")  // Node.js file system
 const ecbData = fs.readFileSync('utils/ECB Exchange Rates.xml').toString('utf8')
-let unitModule = fs.readFileSync('src/units.js').toString('utf8')
+let rates = fs.readFileSync('src/currencyRates.js').toString('utf8')
 
 const rateRegEx = /currency="([^"]+)" rate="([^"]+)"/g
 
@@ -8,11 +8,11 @@ let match;
 while ((match = rateRegEx.exec(ecbData)) !== null) {
   const currencyCode = match[1]
   const rate = match[2]
-  const pos = unitModule.indexOf('"' + currencyCode + `":`)
+  const pos = rates.indexOf('"' + currencyCode + `":`)
   if (pos > -1) {
-    const str = unitModule.slice(pos + 5)
-    unitModule = unitModule.slice(0, pos + 5) + str.replace(/"[^"]+/, '"' + rate)
+    const str = rates.slice(pos + 5)
+    rates = rates.slice(0, pos + 5) + str.replace(/"[^"]+/, '"' + rate)
   }
 }
 
-fs.writeFileSync('src/units.js', unitModule)
+fs.writeFileSync('src/currencyRates.js', rates)
