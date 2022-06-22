3468325e981d9c6f810903c2d6e7cbab70405848
Ron Kok <ronkok@comcast.net>
2022-06-22 14:28:40 -0700

    Link to favicon

--- site/sw.js
+++ site/sw.js
@@ -1,7 +1,7 @@
 
 // Service worker for Hurmet
 
-const version = 'hurmet_2022-06-21-2';
+const version = 'hurmet_2022-06-22';
 // Cache IDs
 const coreID = version + '_core';  // JavaScript & CSS
 const assetsID = version + '_assets'; // images, fonts, CSV, & txt
