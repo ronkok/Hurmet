e14a6b276fc8e5f1cc4e0889e2f04f916bf21b6c
Ron Kok <ronkok@comcast.net>
2022-11-15 13:47:11 -0800

    Blink the correct button when Ctrl-S is called

--- src/menu.js
+++ src/menu.js
@@ -398,7 +398,7 @@ export function saveFileAsJSON(state) {
   const str = JSON.stringify(pruneHurmet(docJSON))
   if (window.showOpenFilePicker && state.doc.attrs.fileHandle) {
     // Use the Chromium File System Access API, so users can click to save a document.
-    const button = document.getElementsByClassName("ProseMirror-menubar").item(0).children[2]
+    const button = document.getElementsByClassName("ProseMirror-menubar").item(0).children[1]
     // Blink the button, so the author knows that a save takes place.
     button.classList.add("ProseMirror-menu-active")
     writeFile(state.doc.attrs.fileHandle, str)
