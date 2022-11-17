1597e840bb7cccccbda321b871811b677b8f1f62
Ron Kok <ronkok@comcast.net>
2022-11-16 18:29:37 -0800

    Support sumAbove() in dataframes

--- site/sw.js
+++ site/sw.js
@@ -1,7 +1,7 @@
 
 // Service worker for Hurmet
 
-const version = 'hurmet_2022-11-09';
+const version = 'hurmet_2022-11-16';
 // Cache IDs
 const coreID = version + '_core';  // JavaScript & CSS
 const assetsID = version + '_assets'; // images, fonts, CSV, & txt
