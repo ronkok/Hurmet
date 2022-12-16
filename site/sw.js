a1c492b59b590b13f433e7878fe2e11b6d9eda16
Ron Kok <ronkok@comcast.net>
2022-12-16 13:52:43 -0800

    Edit path names

--- site/sw.js
+++ site/sw.js
@@ -1,7 +1,7 @@
 
 // Service worker for Hurmet
 
-const version = 'hurmet_2022-12-16-03';
+const version = 'hurmet_2022-12-16-05';
 // Cache IDs
 const coreID = version + '_core';  // JavaScript & CSS
 const assetsID = version + '_assets'; // images, fonts, CSV, & txt
