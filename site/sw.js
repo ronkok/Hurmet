7aaea9d121cd39ecef8ab1af7c70bc65155d0af8
Ron Kok <ronkok@comcast.net>
2022-12-17 10:53:39 -0800

    Revise cache install

--- site/sw.js
+++ site/sw.js
@@ -1,41 +1,41 @@
 // A service worker to enable offline use of Hurmet.app
 
-const version = "hurmet-2022-12-17-04"
+const version = "hurmet-2022-12-17-05"
 
-const addResourcesToCache = async(resources) => {
-  const cache = await caches.open(version)
-  await cache.addAll(resources)
-};
+const coreFiles = [
+  '/index.html',
+  '/manual.html',
+  '/katex.min.js',
+  '/temml.min.js',
+  '/hurmet.min.js',
+  '/prosemirror.min.js',
+  '/demo.js',
+  '/katex.min.css',
+  '/styles.min.css',
+  '/docStyles.min.css',
+  '/images/favicon.png',
+  '/fonts/KaTeX_AMS-Regular.woff2',
+  '/fonts/KaTeX_Caligraphic-Regular.woff2',
+  '/fonts/KaTeX_Main-Bold.woff2',
+  '/fonts/KaTeX_Main-BoldItalic.woff2',
+  '/fonts/KaTeX_Main-Italic.woff2',
+  '/fonts/KaTeX_Main-Regular.woff2',
+  '/fonts/KaTeX_Math-BoldItalic.woff2',
+  '/fonts/KaTeX_Math-Italic.woff2',
+  '/fonts/KaTeX_Size1-Regular.woff2',
+  '/fonts/KaTeX_Size2-Regular.woff2',
+  '/fonts/KaTeX_Size3-Regular.woff2',
+  '/fonts/KaTeX_Size4-Regular.woff2'
+];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    addResourcesToCache([
-      "/",
-      '/index.html',
-      '/manual.html',
-/*      '/katex.min.js',
-      '/temml.min.js',
-      '/hurmet.min.js',
-      '/prosemirror.min.js',
-      '/demo.js',*/
-      '/katex.min.css',
-      '/styles.min.css',
-      '/docStyles.min.css',
-      '/images/favicon.png',
-      '/fonts/KaTeX_AMS-Regular.woff2',
-      '/fonts/KaTeX_Caligraphic-Regular.woff2',
-      '/fonts/KaTeX_Main-Bold.woff2',
-      '/fonts/KaTeX_Main-BoldItalic.woff2',
-      '/fonts/KaTeX_Main-Italic.woff2',
-      '/fonts/KaTeX_Main-Regular.woff2',
-      '/fonts/KaTeX_Math-BoldItalic.woff2',
-      '/fonts/KaTeX_Math-Italic.woff2',
-      '/fonts/KaTeX_Size1-Regular.woff2',
-      '/fonts/KaTeX_Size2-Regular.woff2',
-      '/fonts/KaTeX_Size3-Regular.woff2',
-      '/fonts/KaTeX_Size4-Regular.woff2'
-    ])
-  );
+// On install, cache Javascript & CSS
+self.addEventListener('install', function(event) {
+  event.waitUntil(caches.open(version).then(function(cache) {
+    coreFiles.forEach(function(file) {
+      cache.add(new Request(file));
+    });
+    return cache;
+  }));
 });
 
 const cacheFirst = async(request) => {
