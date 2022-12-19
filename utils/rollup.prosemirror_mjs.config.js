b5f7cb1e8f4bbdfb1392739e3a9f08a20377795e
Ron Kok <ronkok@comcast.net>
2022-12-19 10:39:38 -0800

    Revise build script to accommodate offline

---
+++ utils/rollup.prosemirror_mjs.config.js
@@ -0,0 +1,13 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+  input: "./src/index.js",
+  output: {
+    format: "es",
+    name: "prosemirror",
+    sourcemap: false,
+    file: "preview/prosemirror.mjs"
+  },
+  plugins: [resolve(), commonjs()]
+};
