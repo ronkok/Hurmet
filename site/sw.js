f49cf802a4daf14ac999c1ffdbd5d55d041d25fd
Ron Kok <ronkok@comcast.net>
2022-12-19 18:01:08 -0800

    Simplify sw.js

--- site/sw.js
+++ site/sw.js
@@ -1,11 +1,6 @@
+// A service worker to enable offline use of Hurmet.app
 
-// Service worker for Hurmet
-
-const version = 'hurmet_2022-12-16-11';
-// Cache IDs
-const coreID = version + '_core';  // offline.html & CSS
-const assetsID = version + '_assets'; // images
-const cacheIDs = [coreID, assetsID];
+const version = "hurmet-2022-12-19-05"
 
 const coreFiles = [
   '/offline.html',
@@ -13,13 +8,8 @@ const coreFiles = [
   '/docStyles.min.css'
 ];
 
-//
-// Event Listeners
-//
-
-// On install, cache the core files
 self.addEventListener('install', function(event) {
-  event.waitUntil(caches.open(coreID).then(function(cache) {
+  event.waitUntil(caches.open(version).then(function(cache) {
     coreFiles.forEach(function(file) {
       cache.add(new Request(file));
     });
@@ -27,78 +17,44 @@ self.addEventListener('install', function(event) {
   }));
 });
 
-// On version update, remove old cached files
-self.addEventListener('activate', function(event) {
-  event.waitUntil(caches.keys().then(function(keys) {
-    return Promise.all(keys.filter(function(key) {
-      return !cacheIDs.includes(key);
-    }).map(function(key) {
-      return caches.delete(key);
-    }));
-  }).then(function() {
-    return self.clients.claim();
-  }));
-});
-
-self.addEventListener('fetch', function(event) {
-
-  // Get the request
-  const request = event.request;
-
-  // Ignore non-GET requests
-  if (request.method !== 'GET') { return }
-
-  // CSS
-  // Offline-first, pre-cached
-  if (request.headers.get('Accept').includes('text/css')) {
-    event.respondWith(
-      caches.match(request).then(function(response) {
-        return response || fetch(request).then(function(response) {
-
-          // Return the response
-          return response;
-
-        });
-      })
-    );
-    return;
+const cacheFirst = async(request) => {
+  const responseFromCache = await caches.match(request)
+  if (responseFromCache) {
+    return responseFromCache
   }
+  return fetch(request)
+};
 
-  // HTML from network
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') { return }
+
   if (event.request.mode === 'navigate') {
     event.respondWith((async() => {
       try {
         const networkResponse = await fetch(event.request);
         return networkResponse;
       } catch (error) {
-        // catch is only triggered if an exception is thrown, which is likely
-        // due to a network error.
-        const cache = await caches.open(coreID);
+        const cache = await caches.open(version);
         const cachedResponse = await cache.match("./offline.html");
         return cachedResponse;
       }
     })());
   }
 
-  // Assets: Images & fonts
-  // Offline-first, cache as you browse
-  if (request.headers.get('Accept').includes('image') ||
-      request.headers.get('Accept').includes('font/woff2')) {
-    event.respondWith(
-      caches.match(request).then(function(response) {
-        return response || fetch(request).then(function(response) {
+  event.respondWith(cacheFirst(event.request))
+})
 
-          const copy = response.clone();
-          event.waitUntil(caches.open(assetsID).then(function(cache) {
-            return cache.put(request, copy);
-          }));
+const deleteCache = async(key) => {
+  await caches.delete(key)
+}
 
-          // Return the requested file
-          return response;
+const deleteOldCaches = async() => {
+  const cacheKeepList = [version];
+  const keyList = await caches.keys()
+  const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key))
+  await Promise.all(cachesToDelete.map(deleteCache))
+};
 
-        });
-      })
-    );
-  }
-
-});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(deleteOldCaches())
+})
