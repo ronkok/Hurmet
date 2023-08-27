48170f6dc697f323b1587e8a257683f31e1b4bc4
Ron Kok <ronkok55@outlook.com>
2023-08-26 20:21:32 -0700

    Update currency exchange rates

--- site/sw.js
+++ site/sw.js
@@ -1,6 +1,6 @@
 // A service worker to enable offline use of Hurmet.app
 
-const cacheName = "hurmet-2023-08-24"
+const cacheName = "hurmet-2023-08-26"
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(cacheName));
