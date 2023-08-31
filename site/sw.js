82b6429e5b6554d39541a084e8d2576cd52f2698
Ron Kok <ronkok55@outlook.com>
2023-08-31 14:41:09 -0700

    Update Temml

--- site/sw.js
+++ site/sw.js
@@ -1,6 +1,6 @@
 // A service worker to enable offline use of Hurmet.app
 
-const cacheName = "hurmet-2023-08-26"
+const cacheName = "hurmet-2023-08-31"
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(cacheName));
