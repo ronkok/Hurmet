7977391abf4d4dd4a74b1ac117375c1f270e31c3
Ron Kok <ronkok55@outlook.com>
2023-09-16 17:10:28 -0700

    Fix Mardown read of single-character TeX field

--- site/sw.js
+++ site/sw.js
@@ -1,6 +1,6 @@
 // A service worker to enable offline use of Hurmet.app
 
-const cacheName = "hurmet-2023-09-15"
+const cacheName = "hurmet-2023-09-16"
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(cacheName));
