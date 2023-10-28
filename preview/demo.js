ac85470dff2de8ea9ee4da980d76ee88c94f73da
Ron Kok <ronkok55@outlook.com>
2023-10-27 19:20:47 -0700

    Update Temml & exchange rates

--- preview/demo.js
+++ preview/demo.js
@@ -1459,11 +1459,11 @@ const unitTable = Object.freeze(JSON.parse(`{
 "£":["1","1","0","GBP",[0,0,0,0,0,0,0,1]],
 "'":["0.3048","1","0","0",[1,0,0,0,0,0,0,0]],
 "A":["1","1","0","siSymbol",[0,0,0,1,0,0,0,0]],
-"AUD":["1.6586","1","0","AUD",[0,0,0,0,0,0,0,1]],
+"AUD":["1.6616","1","0","AUD",[0,0,0,0,0,0,0,1]],
 "Adobe point":["0.0254","72","0","0",[1,0,0,0,0,0,0,0]],
 "At":["1","1","0","siSymbol",[0,0,0,0,1,0,1,0]],
 "Australian dollar":["1","1","0","AUD",[0,0,0,0,0,0,0,1]],
-"BRL":["5.3295","1","0","BRL",[0,0,0,0,0,0,0,1]],
+"BRL":["5.2451","1","0","BRL",[0,0,0,0,0,0,0,1]],
 "BTU":["1055.056","1","0","0",[2,1,-2,0,0,0,0,0]],
 "BThU":["1055.056","1","0","0",[2,1,-2,0,0,0,0,0]],
 "Bq":["1","1","0","siSymbol",[0,0,-1,0,0,0,0,0]],
@@ -1472,10 +1472,10 @@ const unitTable = Object.freeze(JSON.parse(`{
 "Btu":["1055.056","1","0","0",[2,1,-2,0,0,0,0,0]],
 "C":["1","1","0","siSymbol",[0,0,1,1,0,0,0,0]],
 "C$":["1","1","0","CAD",[0,0,0,0,0,0,0,1]],
-"CAD":["1.4425","1","0","CAD",[0,0,0,0,0,0,0,1]],
+"CAD":["1.4572","1","0","CAD",[0,0,0,0,0,0,0,1]],
 "CCF":["1","1","0","0",[3,0,0,0,0,0,0,0]],
-"CHF":["0.9492","1","0","CHF",[0,0,0,0,0,0,0,1]],
-"CNY":["7.7278","1","0","CNY",[0,0,0,0,0,0,0,1]],
+"CHF":["0.9502","1","0","CHF",[0,0,0,0,0,0,0,1]],
+"CNY":["7.7143","1","0","CNY",[0,0,0,0,0,0,0,1]],
 "CY":["0.764554857984","1","0","0",[3,0,0,0,0,0,0,0]],
 "Calorie":["4186.8","1","0","0",[2,1,-2,0,0,0,0,0]],
 "Canadian dollar":["1","1","0","CAD",[0,0,0,0,0,0,0,1]],
@@ -1495,7 +1495,7 @@ const unitTable = Object.freeze(JSON.parse(`{
 "Fahrenheit":["5","9","459","0",[0,0,0,0,1,0,0,0]],
 "G":["0.0001","1","0","siSymbol",[-2,-2,-2,-1,0,0,0,0]],
 "GB":["8589934592","1","0","0",[0,0,0,0,0,1,0,0]],
-"GBP":["0.86610","1","0","GBP",[0,0,0,0,0,0,0,1]],
+"GBP":["0.87023","1","0","GBP",[0,0,0,0,0,0,0,1]],
 "Gal":["0.01","1","0","siSymbol",[1,0,-2,0,0,0,0,0]],
 "Gi":["10","12.5663706143592","0","siWord",[0,0,0,0,1,0,1,0]],
 "GiB":["8589934592","1","0","0",[0,0,0,0,0,1,0,0]],
@@ -1503,23 +1503,23 @@ const unitTable = Object.freeze(JSON.parse(`{
 "Gy":["1","1","0","siSymbol",[2,0,-2,0,0,0,0,0]],
 "H":["1","1","0","siSymbol",[2,1,-2,-2,0,0,0,0]],
 "HK$":["1","1","0","HKD",[0,0,0,0,0,0,0,1]],
-"HKD":["8.2687","1","0","HKD",[0,0,0,0,0,0,0,1]],
+"HKD":["8.2426","1","0","HKD",[0,0,0,0,0,0,0,1]],
 "HP":["745.69987158227","1","0","0",[2,1,-3,0,0,0,0,0]],
 "Hong Kong dollar":["1","1","0","HKD",[0,0,0,0,0,0,0,1]],
 "Hz":["1","1","0","siSymbol",[0,0,-1,0,0,0,0,0]],
-"ILS":["4.2515","1","0","ILS",[0,0,0,0,0,0,0,1]],
-"INR":["87.9540","1","0","INR",[0,0,0,0,0,0,0,1]],
+"ILS":["4.2903","1","0","ILS",[0,0,0,0,0,0,0,1]],
+"INR":["87.7510","1","0","INR",[0,0,0,0,0,0,0,1]],
 "Indian Rupee":["1","1","0","INR",[0,0,0,0,0,0,0,1]],
 "Israeli New Shekel":["1","1","0","ILS",[0,0,0,0,0,0,0,1]],
 "J":["1","1","0","siSymbol",[2,1,-2,0,0,0,0,0]],
-"JPY":["158.10","1","0","JPY",[0,0,0,0,0,0,0,1]],
+"JPY":["158.08","1","0","JPY",[0,0,0,0,0,0,0,1]],
 "Japanese Yen":["1","1","0","JPY",[0,0,0,0,0,0,0,1]],
 "Joule":["1","1","0","0",[2,1,-2,0,0,0,0,0]],
 "Julian year":["31557600","1","0","0",[0,0,1,0,0,0,0,0]],
 "Jy":["1e-26","1","0","siSymbol",[0,1,-2,0,0,0,0,0]],
 "K":["1","1","0","0",[0,0,0,0,1,0,0,0]],
 "KiB":["8192","1","0","0",[0,0,0,0,0,1,0,0]],
-"KRW":["1428.23","1","0","KRW",[0,0,0,0,0,0,0,1]],
+"KRW":["1430.38","1","0","KRW",[0,0,0,0,0,0,0,1]],
 "L":["0.001","1","0","siSymbol",[3,0,0,0,0,0,0,0]],
 "Lego stud":["0.008","1","0","siSymbol",[1,0,0,0,0,0,0,0]],
 "MB":["8388608","1","0","0",[0,0,0,0,0,1,0,0]],
@@ -1530,7 +1530,7 @@ const unitTable = Object.freeze(JSON.parse(`{
 "MMscf":["28316.846592","1","0","0",[3,0,0,0,0,0,0,0]],
 "MMscfd":["0.32774128","1","0","0",[3,0,0,0,0,0,0,0]],
 "MT":["1000","1","0","0",[0,1,0,0,0,0,0,0]],
-"MXN":["19.0247","1","0","MXN",[0,0,0,0,0,0,0,1]],
+"MXN":["19.0642","1","0","MXN",[0,0,0,0,0,0,0,1]],
 "Mach":["331.6","1","0","0",[1,0,-1,0,0,0,0,0]],
 "Mbbl":["158.987294928","1","0","0",[3,0,0,0,0,0,0,0]],
 "Mexican Peso":["1","1","0","MXN",[0,0,0,0,0,0,0,1]],
@@ -1560,7 +1560,7 @@ const unitTable = Object.freeze(JSON.parse(`{
 "TeX point":["0.0003515","1","0","0",[1,0,0,0,0,0,0,0]],
 "TiB":["8796093022208","1","0","0",[0,0,0,0,0,1,0,0]],
 "US$":["1","1","0","USD",[0,0,0,0,0,0,0,1]],
-"USD":["1.0565","1","0","USD",[0,0,0,0,0,0,0,1]],
+"USD":["1.0541","1","0","USD",[0,0,0,0,0,0,0,1]],
 "V":["1","1","0","siSymbol",[2,1,-3,-1,0,0,0,0]],
 "VA":["1","1","0","siSymbol",[2,1,-3,0,0,0,0,0]],
 "W":["1","1","0","siSymbol",[2,1,-3,0,0,0,0,0]],
@@ -18835,13 +18835,8 @@ const taggedExpression = (expression, tag, style, leqno) => {
 
   expression = new mathMLTree.MathNode("mtd", [expression]);
   const rowArray = [glue$1(), expression, glue$1()];
-  if (leqno) {
-    rowArray[0].children.push(tag);
-    rowArray[0].style.textAlign = "-webkit-left";
-  } else {
-    rowArray[2].children.push(tag);
-    rowArray[2].style.textAlign = "-webkit-right";
-  }
+  rowArray[leqno ? 0 : 2].classes.push(leqno ? "tml-left" : "tml-right");
+  rowArray[leqno ? 0 : 2].children.push(tag);
   const mtr = new mathMLTree.MathNode("mtr", rowArray, ["tml-tageqn"]);
   const table = new mathMLTree.MathNode("mtable", [mtr]);
   table.style.width = "100%";
@@ -18896,6 +18891,15 @@ function buildMathML(tree, texExpression, style, settings) {
   return math;
 }
 
+const smalls = "acegıȷmnopqrsuvwxyzαγεηικμνοπρςστυχωϕ𝐚𝐜𝐞𝐠𝐦𝐧𝐨𝐩𝐪𝐫𝐬𝐮𝐯𝐰𝐱𝐲𝐳";
+const talls = "ABCDEFGHIJKLMNOPQRSTUVWXYZbdfhkltΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩβδλζφθψ"
+             + "𝐀𝐁𝐂𝐃𝐄𝐅𝐆𝐇𝐈𝐉𝐊𝐋𝐌𝐍𝐎𝐏𝐐𝐑𝐒𝐓𝐔𝐕𝐖𝐗𝐘𝐙𝐛𝐝𝐟𝐡𝐤𝐥𝐭";
+const longSmalls = new Set(["\\alpha", "\\gamma", "\\delta", "\\epsilon", "\\eta", "\\iota",
+  "\\kappa", "\\mu", "\\nu", "\\pi", "\\rho", "\\sigma", "\\tau", "\\upsilon", "\\chi", "\\psi",
+  "\\omega", "\\imath", "\\jmath"]);
+const longTalls = new Set(["\\Gamma", "\\Delta", "\\Sigma", "\\Omega", "\\beta", "\\delta",
+  "\\lambda", "\\theta", "\\psi"]);
+
 const mathmlBuilder$a = (group, style) => {
   const accentNode = group.isStretchy
     ? stretchy.accentNode(group)
@@ -18906,6 +18910,13 @@ const mathmlBuilder$a = (group, style) => {
   } else {
     accentNode.style.mathStyle = "normal";
     accentNode.style.mathDepth = "0";
+    if (needWebkitShift.has(group.label) &&  utils.isCharacterBox(group.base)) {
+      let shift = "";
+      const ch = group.base.text;
+      if (smalls.indexOf(ch) > -1 || longSmalls.has(ch)) { shift = "tml-xshift"; }
+      if (talls.indexOf(ch) > -1  || longTalls.has(ch))  { shift = "tml-capshift"; }
+      if (shift) { accentNode.classes.push(shift); }
+    }
   }
   if (!group.isStretchy) {
     accentNode.setAttribute("stretchy", "false");
@@ -18932,6 +18943,19 @@ const nonStretchyAccents = new Set([
   "\\vec",
   "\\dot",
   "\\mathring"
+]);
+
+const needWebkitShift = new Set([
+  "\\acute",
+  "\\bar",
+  "\\breve",
+  "\\check",
+  "\\dot",
+  "\\ddot",
+  "\\grave",
+  "\\hat",
+  "\\mathring",
+  "\\'", "\\^", "\\~", "\\=", "\\u", "\\.", '\\"', "\\r", "\\H", "\\v"
 ]);
 
 // Accents
@@ -20603,7 +20627,10 @@ rgba(0,0,0,0) 100%);`;
       node.style.marginRight = "0.03889em";
       break
     case "\\sout":
-      node.style["text-decoration"] = "line-through 0.08em solid";
+      node.style.backgroundImage = 'linear-gradient(black, black)';
+      node.style.backgroundRepeat = 'no-repeat';
+      node.style.backgroundSize = '100% 1.5px';
+      node.style.backgroundPosition = '0 center';
       break
     case "\\boxed":
       // \newcommand{\boxed}[1]{\fbox{\m@th$\displaystyle#1$}} from amsmath.sty
@@ -20854,8 +20881,9 @@ const getTag = (group, style, rowNum) => {
     return tag
   } else {
     // AMS automatcally numbered equaton.
-    // Insert a class so the element can be populated by a post-processor.
-    tag = new mathMLTree.MathNode("mtext", [], ["tml-eqn"]);
+    // Insert a class so the element can be populated by a CSS counter.
+    // WebKit will display the CSS counter only inside a span.
+    tag = new mathMLTree.MathNode("mtext", [new Span(["tml-eqn"])]);
   }
   return tag
 };
@@ -29743,7 +29771,7 @@ class Style {
  * https://mit-license.org/
  */
 
-const version = "0.10.16";
+const version = "0.10.17";
 
 function postProcess(block) {
   const labelMap = {};
@@ -29798,9 +29826,9 @@ function postProcess(block) {
  * Parse and build an expression, and place that expression in the DOM node
  * given.
  */
-let render$1 = function(expression, baseNode, options) {
+let render$1 = function(expression, baseNode, options = {}) {
   baseNode.textContent = "";
-  const alreadyInMathElement = baseNode.tagName === "MATH";
+  const alreadyInMathElement = baseNode.tagName.toLowerCase() === "math";
   if (alreadyInMathElement) { options.wrap = "none"; }
   const math = renderToMathMLTree(expression, options);
   if (alreadyInMathElement) {
--- site/sw.js
+++ site/sw.js
@@ -1,6 +1,6 @@
 // A service worker to enable offline use of Hurmet.app
 
-const cacheName = "hurmet-2023-10-19"
+const cacheName = "hurmet-2023-10-27"
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(cacheName));
