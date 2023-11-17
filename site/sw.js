d07103d731470a2347ac7c84d07cc8737d5281cf
Ron Kok <ronkok55@outlook.com>
2023-11-16 19:43:17 -0800

    Publish fallback for fetch()

--- site/sw.js
+++ site/sw.js
@@ -1,6 +1,6 @@
 // A service worker to enable offline use of Hurmet.app
 
-const cacheName = "hurmet-2023-11-15"
+const cacheName = "hurmet-2023-11-16"
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(cacheName));
