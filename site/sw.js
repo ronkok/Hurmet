0b87adadcbe1ae0ec9ef31fc019422b50062da25
Ron Kok <ronkok55@outlook.com>
2023-11-28 08:47:30 -0800

    Adjust nav bar style

--- site/sw.js
+++ site/sw.js
@@ -1,6 +1,6 @@
 // A service worker to enable offline use of Hurmet.app
 
-const cacheName = "hurmet-2023-11-24"
+const cacheName = "hurmet-2023-11-28"
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(cacheName));
