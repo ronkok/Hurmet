9449487cfc2904a1edcfa4d29e92281510d1a102
Ron Kok <ronkok55@outlook.com>
2024-01-27 20:50:51 -0800

    Update sw.js

--- site/sw.js
+++ site/sw.js
@@ -1,6 +1,6 @@
 // A service worker to enable offline use of Hurmet.app
 
-const cacheName = "hurmet-2024-01-20-01"
+const cacheName = "hurmet-2024-01-27"
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(cacheName));
