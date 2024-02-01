69e580a12031b624deaf5b3a1093036efd76bac3
Ron Kok <ronkok55@outlook.com>
2024-01-31 19:39:56 -0800

    New check

--- site/sw.js
+++ site/sw.js
@@ -1,6 +1,6 @@
 // A service worker to enable offline use of Hurmet.app
 
-const cacheName = "hurmet-2024-01-31-23"
+const cacheName = "hurmet-2024-01-31-24"
 
 const addResourcesToCache = async(resources) => {
   const cache = await caches.open(cacheName)
@@ -28,6 +28,7 @@ self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
     // Open the cache
     event.respondWith(caches.open(cacheName).then((cache) => {
+      console.log("First")
       if (!navigator.onLine) {
         console.log("Hi")
         // Put up the offline page
