c258b06f0cde3351ebbf223a270ea84b8f9112df
Ron Kok <ronkok55@outlook.com>
2024-07-19 20:20:18 -0700

    Update service worker

--- site/sw.js
+++ site/sw.js
@@ -1,6 +1,6 @@
 // A service worker to enable offline use of Hurmet.app
 
-const cacheName = "hurmet-2024-07-13"
+const cacheName = "hurmet-2024-07-19"
 
 const addResourcesToCache = async(resources) => {
   const cache = await caches.open(cacheName)
