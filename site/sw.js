332787ec480e6c45e6751de4d5a6c15856de3ae8
Ron Kok <ronkok55@outlook.com>
2024-11-09 09:42:19 -0800

    Include user's manual in offline page.

--- site/sw.js
+++ site/sw.js
@@ -1,21 +1,40 @@
 // A service worker to enable offline use of Hurmet.app
 
-const cacheName = "hurmet-2024-11-8"
+const cacheName = "hurmet-2024-11-9"
 
 const addResourcesToCache = async(resources) => {
   const cache = await caches.open(cacheName)
   await cache.addAll(resources)
 }
 
-// Pre-cache the offline page, JavaScript, CSS, and fonts.
+// Pre-cache the offline page, manual, unit page, JavaScript, CSS, and fonts.
 self.addEventListener("install", (event) => {
   event.waitUntil(
     addResourcesToCache([
       '/offline.html',
       '/prosemirror.min.js',
+      '/demo.min.js',
       '/styles.min.css',
+      '/docStyles.min.css',
       '/latinmodernmath.woff2',
-      '/Temml.woff2'
+      '/Temml.woff2',
+      '/manual.html',
+      '/unit-definitions.html',
+      '/images/favicon.svg',
+      '/images/assignment-railroad.svg',
+      '/images/statement-railroad.svg',
+      '/images/identifier-railroad.svg',
+      '/images/NumberRailroad.svg',
+      '/images/unit-railroad.svg',
+      '/images/if-railroad.svg',
+      '/images/test-railroad.svg',
+      '/images/sinx.svg',
+      '/images/spiral.svg',
+      '/images/for-loop-railroad.svg',
+      '/images/return-railroad.svg',
+      '/images/throw-railroad.svg',
+      '/images/print-railroad.svg',
+      '/images/one-liner-railroad.svg'
     ])
   )
 })
@@ -36,18 +55,25 @@ const cleanResponse = response => {
 
 // The purpose of this worker is to enable offline use, not to speed startup.
 // Hurmet is in active development and I always want to load the most current JS.
-// So, in most cases, go to the network first. If network is unavailable, get the cache.
+// So, in most cases, go to the network first. If the network is unavailable, get the cache.
 self.addEventListener('fetch', event => {
   if (event.request.mode === 'navigate') {
     // Open the cache
     event.respondWith(caches.open(cacheName).then(cache => {
-      // Go to the network first
-      return fetch(event.request.url).then(fetchedResponse => {
-        return cleanResponse(fetchedResponse)
-      }).catch(() => {
-        // If the network is unavailable, put up the offline page
-        return cache.match('/offline.html').then(response => cleanResponse(response))
-      });
+      // Go to the cache first only for the user's manual.
+      return cache.match(event.request.url).then(cachedResponse => {
+        // Return a cached response if we have one
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        // If not the manual, go to the network first
+        return fetch(event.request.url).then(fetchedResponse => {
+          return cleanResponse(fetchedResponse)
+        }).catch(() => {
+          // If the network is unavailable, put up the offline page
+          return cache.match('/offline.html').then(response => cleanResponse(response))
+        });
+      })
     }));
   } else if (event.request.destination === 'script' || event.request.destination === 'style') {
     // This also calls for network first. Open the cache.
@@ -60,8 +86,8 @@ self.addEventListener('fetch', event => {
         return cache.match(event.request.url)
       });
     }));
-  } else if (event.request.destination === 'font') {
-    // Fonts are the only files for which we go cache-first.
+  } else if (event.request.destination === 'font' || event.request.destination === 'image') {
+    // Fonts and images are the only files for which we go cache-first.
     event.respondWith(caches.open(cacheName).then(cache => {
       // Go to the cache
       return cache.match(event.request.url).then(cachedResponse => {
