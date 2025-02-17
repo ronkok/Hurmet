a396dc2f3c02bbfad45ed3fb04f1115914c2118c
Ron Kok <ronkok55@outlook.com>
2025-02-16 19:40:51 -0800

    Update currency exchange rates

--- site/sw.js
+++ site/sw.js
@@ -1,6 +1,6 @@
 // A service worker to enable offline use of Hurmet.app
 
-const cacheName = "hurmet-2025-02-14"
+const cacheName = "hurmet-2025-02-16"
 
 const addResourcesToCache = async(resources) => {
   const cache = await caches.open(cacheName)
