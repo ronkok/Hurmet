50dbe83b6c2018602fc7d09fe280ace851a67f18
Ron Kok <ronkok55@outlook.com>
2025-03-15 13:00:34 -0700

    Fix sample id's

--- site/sw.js
+++ site/sw.js
@@ -1,6 +1,6 @@
 // A service worker to enable offline use of Hurmet.app
 
-const cacheName = "hurmet-2025-03-15"
+const cacheName = "hurmet-2025-03-15-1"
 
 const addResourcesToCache = async(resources) => {
   const cache = await caches.open(cacheName)
