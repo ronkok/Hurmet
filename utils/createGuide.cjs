13d0b7d983d0f0bfab08134122780d6cee08e47b
Ron Kok <ronkok55@outlook.com>
2025-05-03 10:31:52 -0700

    Pass an argument to createGuide.cjs

--- utils/createGuide.cjs
+++ utils/createGuide.cjs
@@ -4,13 +4,17 @@ const hurmet = require('./hurmet.cjs')
 const temml = require('./temml.cjs');
 // eslint-disable-next-line no-undef
 globalThis.temml = temml;
-const titleRegEx = /([^.\\/]+)\.md$/;  // A helper.
+
+// This file is typically called as per:   node utils\createGuide.cjs <title>
+
+// Get the document title from the args passed to the call to node.js
+const args = process.argv.slice(2) // Skip the first two elements (node & script path)
+const title = args[0];
 
 // The main function has to be async.
 (async function main() {
-  const inputPath = 'docs/Why-Hurmet.md'
-  const title = titleRegEx.exec(inputPath)[1];
-  const outputPath = `site/guides/${title}.html`
+  const inputPath = `docs/${title}.md`
+  const outputPath = `./site/guides/${title}.html`
   // Read the file.
   const md = fs.readFileSync(inputPath).toString('utf8')
   // Run the calculations and convert to HTML.
