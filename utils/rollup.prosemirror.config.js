7977d8d8e7580e0257a4979d45c64ea403b7c1f4
ronkok <ronkok55@outlook.com>
2026-03-22 13:06:30 -0700

    Modify Rollup config to force LF endings

--- utils/rollup.prosemirror.config.js
+++ utils/rollup.prosemirror.config.js
@@ -1,6 +1,18 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 
+function forceLf() {
+  return {
+    name: 'force-lf',
+    renderChunk(code) {
+      return {
+        code: code.replace(/\r\n?/g, '\n'),
+        map: null
+      };
+    }
+  };
+}
+
 export default {
   input: "./src/index.js",
   output: {
@@ -9,5 +21,5 @@ export default {
     sourcemap: false,
     file: "preview/prosemirror.js"
   },
-  plugins: [resolve(), commonjs()]
+  plugins: [resolve(), commonjs(), forceLf()]
 };
