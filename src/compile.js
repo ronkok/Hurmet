912d3535243270a302c08f6e2466fa0ceb60c8bd
ronkok <ronkok55@outlook.com>
2026-04-16 09:46:05 -0700

    Fix percentage selector bug

--- src/compile.js
+++ src/compile.js
@@ -35,7 +35,7 @@ import { unitFromUnitName } from "./units"
 
 const containsOperator = /[+\-×·*∘⌧/^%‰&√!¡|‖&=<>≟≠≤≥∈∉⋐∧∨⊻¬]|\xa0(function|mod|\\atop|root|sum|abs|cos|sin|tan|acos|asin|atan|sec|csc|cot|asec|acsc|acot|exp|log|ln|log10|log2|cosh|sinh|tanh|sech|csch|coth|acosh|asinh|atanh|asech|acsch|acoth|gamma|Γ|lgamma|logΓ|lfact|cosd|sind|tand|acosd|asind|atand|secd|cscd|cotd|asecd|acscd|acotd|real|imag|angle|Char|round|sqrt|sign|\?{}|%|⎾⏋|⎿⏌|\[\]|\(\))\xa0/
 const mustDoCalculation = /^(?:``.+``|(?:[£¥\u20A0-\u20CF]|(?:[ACR]|HK|US)?\$)?(?:\?{1,2}|@{1,2}|%{1,3}|!{1,2})[^=!(?@!{})]*)$/
-const percentOperatorRegEx = /((?:\?{1,2}|@{1,2}|%{1,2}|!{1,2}) *)%/
+const percentOperatorRegEx = /((?:\?{1,2}|@{1,2}|%%|!{1,2}) *)%/
 const assignDataFrameRegEx = /^[^=]+=\s*``[\s\S]+``\s*$/
 const currencyRegEx = /^[$£¥\u20A0-\u20CF]/
 // eslint-disable-next-line max-len
